5206c571cd112880bcd15dd33a6a2b1ef8710960
heitor pires <heitorfernandes_3@hotmail.com>
2024-08-03 18:07:21 -0300

    feat: create home page

---
+++ src/assets/IconBreath/index.ts
@@ -0,0 +1,15 @@
+import BreathIcon from "./breath.png";
+import AnxietyIcon from "./anxietyBreath.png";
+import BothLungIcon from "./bothLung.png";
+import BreathDeepIcon from "./breathDeep.png";
+import LungBreathIcon from "./lungBreath.png";
+import LungSideIcon from "./lungSide.png";
+
+export {
+  BreathIcon,
+  AnxietyIcon,
+  BothLungIcon,
+  BreathDeepIcon,
+  LungBreathIcon,
+  LungSideIcon,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        light: {
+          background: "#e8e7e9ff",
+          textColor: "#000000",
+        },
+        dark: {
+          background: "#0f0f0fff",
+          textColor: "#FFFFFF",
+        },
       },
+      fontFamily: {
+        
+      }
     },
   },
   plugins: [],
